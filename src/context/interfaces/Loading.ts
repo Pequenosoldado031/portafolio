a99523986f40ceb5e7d89af795891553433db3ab
Daniel Stiven Rivera <pequenosoldado031@gmail.com>
2022-08-16 08:21:10 -0500

    Revert "Feature/custom styles"

--- src/context/interfaces/Loading.ts
+++
@@ -1,3 +0,0 @@
-export interface LoadingInterface {
-  isLoading: boolean;
-}
--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.scss';
+import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {LoadingProvider} from "./context/Loading/LoadingProvider";
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
-
 root.render(
   <React.StrictMode>
-    <LoadingProvider>
-      <App />
-    </LoadingProvider>
+    <App />
   </React.StrictMode>
 );
 
